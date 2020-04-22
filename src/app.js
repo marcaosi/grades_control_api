c4b8129c3711c5b3abc2a84b6837d88390b9ab54
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2020-04-21 21:40:36 -0300

    creating method post

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import model from './model/Grade'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Grades Control API. Welcome!');
 });
@@ -17,6 +19,23 @@ app.get('/grade/:id', async (req, res) => {
     const grade = await model.findOne(req.param.id)
 
     res.send(JSON.stringify(grade))
+})
+
+app.post('/grade', async (req, res) => {
+    try{
+        model.validate(req.body, [
+            ["student", "required|string"],
+            ["subject", "required|string"],
+            ["type"   , "required|string"],
+            ["value"  , "required|number"]
+        ])
+        const grade = await model.save(req.body)
+    
+        res.send(JSON.stringify(grade))
+    }catch(err){
+        console.log(err.message)
+        res.status(400).send(JSON.stringify({err: err.message}))
+    }
 })
   
 app.listen(3000, () => {
--- src/model/Grade.js
+++ src/model/Grade.js
@@ -2,18 +2,68 @@ import fs from 'fs'
 import { promisify, isNumber } from 'util'
 
 const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
 
 const getDataBase = async () => {
     const data = await readFile("./database.json")
     return JSON.parse(data)
 }
 
-const GradeModel = {
-    save: (grade) => {
+const setDataBase = async (data) => {
+    await writeFile("./database.json", JSON.stringify(data))
+}
 
+const validateFunctions = {
+    required : (value) => {
+        if(value != undefined && value != null && value != ""){
+            return true
+        }else{
+            throw new Error("Campo vazio.")
+        }
     },
-    validate: (grade, criteria) => {
+    string: (value) => {
+        if (typeof value === "string"){
+            return true
+        }else {
+            throw new Error("Campo deve ser uma string.")
+        }
+    },
+    number: (value) => {
+        if(typeof value === "number"){
+            return true
+        }else{
+            throw new Error("Campo deve ser um número.")
+        }
+    }
+}
 
+const GradeModel = {
+    save: async (grade) => {
+        const db = await getDataBase()
+        grade.id = db.nextId++
+        grade.timestamps = new Date()
+        db.grades.push(grade)
+
+        setDataBase(db)
+    },
+    validate: (grade, criteria = []) => {
+        if(!grade){
+            throw new Error("Deve enviar dados no corpo da requisição")
+        }
+        criteria.forEach(criterio => {
+            const field = criterio[0]
+            const stringValidations = criterio[1]
+            const validations = stringValidations.split("|")
+
+            validations.forEach(validation => {
+                let validationFunction = validateFunctions[validation]
+                if(validateFunctions == null){
+                    throw new Error("Validação não é possível.")
+                }
+
+                validationFunction(grade[field])
+            })
+        })
     },
     find: async (criteria = []) => {
         const db = await getDataBase()
